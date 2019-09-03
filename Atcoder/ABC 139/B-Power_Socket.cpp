897d8bce8093952cbf3813da18626519c12ae2e8
Manas Singh <Manassingh557@gmail.com>
2019-09-03 22:17:02 +0530

    Create B-Power_Socket.cpp

---
+++ Atcoder/ABC 139/B-Power_Socket.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    a--;b--;
+    int ans=(a+b-1)/a;
+    cout<<ans<<endl;
+}
